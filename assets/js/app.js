8f4bca3fdbc046c4c0d1db403ade27886dc5e31e
mbolonzi-design <mbolonzi.design@gmail.com>
2022-05-18 22:27:58 +0300

    add questions object

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+const questions = [
+    {
+        question:"Which JavaScript method is used to write HTML output?",
+        A: "console.log()",
+        B: "document.output()",
+        C: "document.write()",
+        D: "document.writeHTML()",
+        correct: "C",
+    },
+    {
+        question: "Which JavaScript method is used to access an HTML element?",
+        A: "getElementById(id)",
+        B:  "getElement(id)",
+        C: "elementById(id)",
+        D: "getElementById()",
+        correct: "A",            
+    },
+    {
+        question: "The 'function' and 'var' are known as?",
+        A: "Keywords",
+        B: "Data Types",
+        C: "Prototypes",
+        D: "Declaration statements",
+        correct: "D",
+    },
+    {
+        question: "Which type of language is JavaScript?",
+        A: "High-level",
+        B: "Object-Based",
+        C: "Object-Oriented",
+        D: "Assembly-language",
+    },
+];
