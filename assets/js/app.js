14341e448f200318df1ee8498c0561e27e1895b9
mbolonzi-design <mbolonzi.design@gmail.com>
2022-05-18 22:45:22 +0300

    declare js constants

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,46 @@
 const questions = [
     {
         question:"Which JavaScript method is used to write HTML output?",
-        A: "console.log()",
-        B: "document.output()",
-        C: "document.write()",
-        D: "document.writeHTML()",
-        correct: "C",
+        ans1: "console.log()",
+        ans2: "document.output()",
+        ans3: "document.write()",
+        ans4: "document.writeHTML()",
+        correct: "ans3",
     },
     {
         question: "Which JavaScript method is used to access an HTML element?",
-        A: "getElementById(id)",
-        B:  "getElement(id)",
-        C: "elementById(id)",
-        D: "getElementById()",
-        correct: "A",            
+        ans1: "getElementById(id)",
+        ans2:  "getElement(id)",
+        ans3: "elementById(id)",
+        ans4: "getElementById()",
+        correct: "ans1",            
     },
     {
         question: "The 'function' and 'var' are known as?",
-        A: "Keywords",
-        B: "Data Types",
-        C: "Prototypes",
-        D: "Declaration statements",
-        correct: "D",
+        ans1: "Keywords",
+        ans2: "Data Types",
+        ans3: "Prototypes",
+        ans4: "Declaration statements",
+        correct: "ans4",
     },
     {
         question: "Which type of language is JavaScript?",
-        A: "High-level",
-        B: "Object-Based",
-        C: "Object-Oriented",
-        D: "Assembly-language",
+        ans1: "High-level",
+        ans2: "Object-Based",
+        ans3: "Object-Oriented",
+        ans4: "Assembly-language",
+        correct: "ans2"
     },
 ];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".anwer");
+const questionEl = document.getElementById("question");
+const ans01 = document.getElementById("ans01");
+const ans02 = document.getElementById("ans02");
+const ans03 = document.getElementById("ans03");
+const ans04 = document.getElementById("ans04");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
